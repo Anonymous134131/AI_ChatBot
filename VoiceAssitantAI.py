38d335e207ac2216d3c7e260a74fcc345d1d60a1
Dhruv Modi <dhruvkmodi@gmail.com>
2022-10-28 12:56:43 -0600

    Edited Code - Dhruv

--- VoiceAssitantAI.py
+++ VoiceAssitantAI.py
@@ -1,29 +1,33 @@
-from queue import Empty
 import speech_recognition as s
 import pyttsx3 
 import datetime
+import wikipedia
+import webbrowser
+import os
+import time
+import subprocess
+import wolframalpha
 
 texttospeech = pyttsx3.init('sapi5')
-texttospeech.setProperty('voice', texttospeech.getProperty('voices')[1].id)
+texttospeech.setProperty('voice', texttospeech.getProperty('voices')[0].id)
 
 def speak(text):
     texttospeech.say(text)
     texttospeech.runAndWait()
 
 def takeCommand():
-    recognizer= s.Recognizer()
-    microphone = s.Microphone()
-    while True:
+    r=s.Recognizer()
+    with s.Microphone() as source:
         print("Listening...")
-        audio= recognizer.listen(microphone)
+        audio=r.listen(source)
+
         try:
-            statement=recognizer.recognize_google(audio,language='en-in')
+            statement=r.recognize_google(audio,language='en-in')
             print(f"user said:{statement}\n")
 
         except Exception as e:
             speak("Pardon me, please say that again")
             return "None"
-        
         return statement
 
 if __name__=='__main__':
@@ -56,3 +60,40 @@ if __name__=='__main__':
             speak('Good bye')
             print('Good bye')
             break
+        if 'wikipedia' in statement:
+            speak('Searching Wikipedia...')
+            statement =statement.replace("wikipedia", "")
+            results = wikipedia.summary(statement, sentences=3)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        elif 'open youtube' in statement:
+            webbrowser.open_new_tab("https://www.youtube.com")
+            speak("youtube is open now")
+            time.sleep(5)
+
+        elif 'open google' in statement:
+            webbrowser.open_new_tab("https://www.google.com")
+            speak("Google chrome is open now")
+            time.sleep(5)
+
+        elif 'open gmail' in statement:
+            webbrowser.open_new_tab("gmail.com")
+            speak("Google Mail open now")
+            time.sleep(5)
+
+        elif 'ask' in statement:
+            speak("I can answer omputational and geographical questions")
+            question=takeCommand()
+            appid = "2WGHR5-HHQ78XU44V"
+            client = wolframalpha.Client("2WGHR5-HHQ78XU44V")
+            res = client.query(question)
+            answer = next(res.results).text
+            speak(answer)
+            print(answer)
+        
+        elif "log off" in statement or "signout" in statement:
+            speak("Your PC will log off in 10 seconds, please close all you applications")
+            subprocess.call(["shutdown", "/h"])
+
+time.sleep(3)
